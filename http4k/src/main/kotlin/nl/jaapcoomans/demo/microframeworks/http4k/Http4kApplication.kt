41c9f8d1496874855ea3879b2453bfa8b0c45499
Jaap Coomans <jaapcoomans@gmail.com>
2021-05-24 20:59:10 +0200

    Upgrade http4k to 4.9.1.0 on JDK 16 and Kotlin 1.5.0

--- http4k/src/main/kotlin/nl/jaapcoomans/demo/microframeworks/http4k/Http4kApplication.kt
+++ http4k/src/main/kotlin/nl/jaapcoomans/demo/microframeworks/http4k/Http4kApplication.kt
@@ -9,6 +9,7 @@ import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.filter.AllowAllOriginPolicy
 import org.http4k.filter.CorsPolicy
 import org.http4k.filter.ServerFilters
 import org.http4k.routing.bind
@@ -16,6 +17,7 @@ import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import java.util.Locale
 
 fun main() {
     val todoRepository = InMemoryTodoRepository()
@@ -32,7 +34,7 @@ fun main() {
             "/todos/{id}" bind PATCH to todoRestController::patchTodo,
             "/todos/{id}" bind DELETE to todoRestController::deleteTodo
     ).withFilter(ServerFilters.Cors(CorsPolicy(
-            origins = listOf("*"),
+            originPolicy = AllowAllOriginPolicy,
             headers = listOf("Content-Type"),
             methods = listOf(GET, POST, DELETE, PATCH)
     )))
@@ -44,5 +46,9 @@ fun main() {
 
 val helloController = fun(req: Request): Response {
     return Response(OK).body("Hello, ${req.path("name")?.capitalize()}!")
+}
+
+fun String.capitalize(): String {
+    return this.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 }
 
